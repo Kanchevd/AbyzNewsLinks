831dd8e773df78c8301eae9b80382867cd6f5704
Daniel Kanchev <daniel.kanchev@adata.pro>
2020-12-18 12:29:16 +0200

    found special countries formatted the path to extract region/country

--- abyznews/spiders/abyz.py
+++ abyznews/spiders/abyz.py
@@ -1,6 +1,6 @@
 import scrapy
 import time
-
+import re
 
 class AbyzSpider(scrapy.Spider):
     name = 'abyz'
@@ -15,3 +15,26 @@ class AbyzSpider(scrapy.Spider):
         countries = response.xpath("(//table)[6]//a/@href").getall()
         for country in countries:
             print(country)
+
+        yield from response.follow_all(countries, callback=self.parse_country)
+
+    def parse_country(self, response):
+        path = response.xpath("(//table)[3]//text()").getall()
+        path = [text for text in path if text.strip() and text.strip() != '>']
+        path = [re.sub('[(\n)?.!/;:>]', '', text) for text in path]
+        if len(path) < 3:
+            print(path[-1] + ' SECOND')
+            return
+
+        if len(path) == 3:
+            print(path[-1] + ' THIRD')
+            return
+
+        country = path[3]
+
+        header = response.xpath("(//table)[4]//text()").getall()
+        header = [text for text in header if text.strip()]
+        status = 'Scrape now' if header[0] == "Media Type" else 'Go Further'
+
+        if status == 'Go Further':
+            print(country + ' ' + status)
