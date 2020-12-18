26854ab5afdef8d0ef887006bdb4f6abfa79a635
Daniel Kanchev <daniel.kanchev@adata.pro>
2020-12-18 11:52:30 +0200

    got links of all countries

--- abyznews/spiders/abyz.py
+++ abyznews/spiders/abyz.py
@@ -1,10 +1,17 @@
 import scrapy
+import time
 
 
 class AbyzSpider(scrapy.Spider):
     name = 'abyz'
     allowed_domains = ['abyznewslinks.com']
-    start_urls = ['http://abyznewslinks.com/']
+    start_urls = ['http://www.abyznewslinks.com/allco.htm']
 
     def parse(self, response):
-        pass
+        filename = 'file.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+        countries = response.xpath("(//table)[6]//a/@href").getall()
+        for country in countries:
+            print(country)
